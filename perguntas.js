699438e479e534ad70851e419ebaf9ea0b8fa0ba
apaixaols <140622198+apaixaols@users.noreply.github.com>
2024-10-29 08:29:45 -0300

    29/10

---
+++ perguntas.js
@@ -0,0 +1,62 @@
+criaCartao(
+    'Programação',
+    'para que serve uma div?',
+    'são elementos usados ​​para definir partes de um documento'
+)
+criaCartao(
+    'Português',
+    'O que é um verbo intransitivo?',
+    'É um verbo que não precisa de complemento.'
+)
+criaCartao(
+    'História',
+    'Quem foi Joana D’Arc?',
+    'Joana d’Arc foi uma camponesa e santa francesa canonizada pela Igreja Católica considerada uma heroína da França pelos seus feitos durante a Guerra dos Cem Anos.'
+)
+criaCartao(
+    'Geografia',
+    'O que é indústria 4.0?',
+    'É o uso da inteligência artificial nos processos industriais'
+)
+criaCartao(
+    'Matemática',
+    'Qual é o lado do triângulo retângulo chamado de hipotenusa?',
+    'o lado oposto ao ângulo de 90°.'
+) 
+criaCartao(
+    'Sociologia',
+    'O que que foi o neoliberalismo?',
+    'É um modo de política econômica que defende a intervenção mínima do Estado na economia.'
+)
+criaCartao(
+    'Física',
+    'qual a primeira lei de Newton?',
+    'lei da inércia'
+)
+criaCartao(
+    'Empreendedorismo',
+    'o que significa a sigla CLT?',
+    'consolidação das leis do trabalho'
+)
+criaCartao(
+    'Biologia',
+    'Quem é considerado o pai da genética?',
+    'Mendel'
+)
+criaCartao(
+    'Química',
+    'o que é um hidrocarboneto?',
+    'substâncias orgânicas que possuem apenas carbono e  hidrogênio em sua constituição.'
+)
+criaCartao(
+    'Inglês',
+    'para que serve a estrutura be+going+to?',
+    'indicar ações futuras.'
+
+)
+criaCartao(
+    'Biotecnologia',
+    'para que serve a biotecnologia dourada ?',
+    'é o tipo de biotecnologia voltada para bioinformática e nanotecnologia.'
+
+)
