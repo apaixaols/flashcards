801ba3954386e802db9041f8fc83223c2c50888e
apaixaols <140622198+apaixaols@users.noreply.github.com>
2024-11-19 07:53:12 -0300

    r

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ function criaCartao(categoria, pergunta, resposta) {
                     respostaEstaVisivel = !repostaEstaVisivel
                     cartao.classList.toggle('active',respostaEstaVisivel)
                 }
+                cartao.addEventListener('click', viraCartao)
                 let container = document.getElementById('container')
        container.appendChild(cartao)
 }
